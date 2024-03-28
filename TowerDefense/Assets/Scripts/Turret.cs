6fc44a9f1d9f6243795ebd230a02bfbcc95938b4
Mauro Moreno <mmauromoreno1@gmail.com>
2024-03-28 18:18:33 -0300

    Implemented turret rotation towards the enemy

--- TowerDefense/Assets/Scripts/Turret.cs
+++ TowerDefense/Assets/Scripts/Turret.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class Turret : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{   
+    [Header("References")]
+    [SerializeField] private Transform turretRotationPoint;
+    [SerializeField] private LayerMask enemyMask;
+
+    [Header("Attributes")]
+    [SerializeField] private float targetRange = 3f;
+    [SerializeField] private float rotationSpeed = 5f;
+
+    private Transform target;
+
+    private void Update()
     {
+        if (target == null)
+        {
+            FindTarget();
+            return;
+        }
+
+        RotateTowardsTarget();
         
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmosSelected()
     {
-        
+        Handles.color = Color.yellow;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetRange);
+    }
+
+    private void FindTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetRange, (Vector2)transform.position, 0f, enemyMask);
+
+        if (hits.Length > 0f)
+        {
+            target = hits[0].transform;
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        //* Targets the first enemy that enters the turret range
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+
+        //* Rotates the cannon towards the enemy
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private bool CheckTargetIsInRange()
+    {
+        return Vector2.Distance(target.position, transform.position) <= targetRange;
     }
 }
