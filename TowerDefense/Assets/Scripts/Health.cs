2c4db69564bba2f778354371b4731ff6070f9290
Mauro Moreno <mmauromoreno1@gmail.com>
2024-04-03 21:45:22 -0300

    Enemies increase currency

--- TowerDefense/Assets/Scripts/Health.cs
+++ TowerDefense/Assets/Scripts/Health.cs
@@ -6,14 +6,19 @@ public class Health : MonoBehaviour
 {
     [Header("Attributes")]
     [SerializeField] private int hitpoints = 2;
+    [SerializeField] private int goldValue = 50;
+
+    private bool isDestroyed = false;
 
     public void TakeDamage(int damage)
     {
         hitpoints -= damage;
 
-        if (hitpoints <= 0)
+        if (hitpoints <= 0 && !isDestroyed)
         {
             EnemySpawner.onEnemyDestroy.Invoke();
+            LevelManager.instance.IncreaseCurrency(goldValue);
+            isDestroyed = true;
             Destroy(this.gameObject);
         }
     }
--- TowerDefense/Assets/Scripts/LevelManager.cs
+++ TowerDefense/Assets/Scripts/LevelManager.cs
@@ -9,11 +9,37 @@ public class LevelManager : MonoBehaviour
     public Transform[] waypoints;
     public Transform startPoint;
 
+    public int currency;
+
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        currency = 100;
+    }
+
+    public void IncreaseCurrency(int amount)
+    {
+        currency += amount;
+    }
+
+    public bool DecreaseCurrency(int amount)
+    {
+        if (amount <= currency)
+        {
+            currency -= amount;
+            return true;
+        }
+        else
+        {
+            Debug.Log("You don't have enough money");
+            return false;
         }
     }
 }
