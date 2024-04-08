540e4838927af19b88e77d2f4f5825f0c13c5805
Mauro Moreno <mmauromoreno1@gmail.com>
2024-04-08 17:56:25 -0300

    Implemented currency update in screen UI

---
+++ TowerDefense/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] TextMeshProUGUI currencyUI;
+
+    private void OnGUI()
+    {
+        currencyUI.text = LevelManager.instance.currency.ToString();
+    }
+}
